a971e49664cb1acb0f80a2b0d29c52360e22e61a
ledinemax <ledinemax@gmail.com>
2023-07-28 17:33:13 -0300

    add parte de numero em contato

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 
 //FIREBASE
 import database from '../services/firebase'
-import { ref, push, set} from 'firebase/database'
+import { ref, push, set } from 'firebase/database'
 
 import Header from '../components/Header'
 import styles from '../style/pages/contato.module.css'
+import contato from '../assets/contato.png'
 
 
 const Contato = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
+  const [number, setNumber] = useState('')
 
 
   function handleInputName(e) {
@@ -22,11 +24,21 @@ const Contato = () => {
     setEmail(e.target.value)
   }
 
+  function handleInputNumber(e) {
+    const inputNumber = e.target.value; 
+  
+    const onlyNumber = inputNumber.replace(/[^0-9+()\s-]/g, '');
+    if (onlyNumber.length <= 11 ) {
+      setNumber(onlyNumber);
+    }
+  }
+
   function handleInputMessage(e) {
     setMessage(e.target.value)
   }
 
-  function handleSubmit(e){
+
+  function handleSubmit(e) {
     e.preventDefault()
 
     const messageListRef = ref(database, 'messages') //cria coleção no DB do Firebase
@@ -34,46 +46,66 @@ const Contato = () => {
     set(newMessageRef, {
       name: name,
       email: email,
-      text: message
-    }) 
-    
+      text: message,
+      number: number
+    })
+
     setName('')
     setEmail('')
     setMessage('')
+    setNumber('')
   }
 
   return (
     <>
       <Header className={styles.headerContato}
-        title="Vamos hablar?"
-        // image={contato}
+        title="Onde nos achar"
+        text='Aqui você sabe onde nos achar, seja por endereço, 
+        redes sociais, e até mesmo para nos enviar uma mensagem para entrarmos em contato.'
+        image={contato}
       />
-      
-      <main className={styles.formContainer}>
 
-        <form className={styles.form} onSubmit={handleSubmit}>
+      <main className={styles.contactContainer}>
+        <div className={styles.contactText}>
+          <h2 className={styles.contactTitle}>Vamos conversar?</h2>
+          <p> Queremos ouvir você! Se você tem alguma dúvida, sugestão,
+            interesse em nossos projetos ou deseja fazer parte de nossa
+            causa, entre em contato conosco. Sua opinião é valiosa para o
+            aprimoramento contínuo de nossas ações em prol da limpeza e
+            manutenção das águas.</p>
+        </div>
 
-          <input className={styles.formInput}
-            type="text"
-            placeholder='Digite seu nome'
-            onChange={handleInputName}
-            value={name}
-          />
-          <input className={styles.formInput}
-            type="email"
-            placeholder='Digite seu email'
-            onChange={handleInputEmail}
-            value={email}
-          />
-          <textarea className={styles.formInput}
-            placeholder='Digite sua mensagem'
-            onChange={handleInputMessage}
-            value={message}
-          />
-          <button className={styles.formButton}
-            type='submit'>Enviar mensagem
-          </button>
-        </form>
+        <div className={styles.formContainer}>
+          <form className={styles.form} onSubmit={handleSubmit}>
+
+            <input className={styles.formInput}
+              type="text"
+              placeholder='Digite seu nome'
+              onChange={handleInputName}
+              value={name}
+            />
+            <input className={styles.formInput}
+              type="email"
+              placeholder='Digite seu email'
+              onChange={handleInputEmail}
+              value={email}
+            />
+            <input className={styles.formInput}
+              type="text"
+              value={number}
+              onChange={handleInputNumber}
+              placeholder="Digite o número de celular"
+            />
+            <textarea className={styles.formInput}
+              placeholder='Digite sua mensagem'
+              onChange={handleInputMessage}
+              value={message}
+            />
+            <button className={styles.formButton}
+              type='submit'>Enviar mensagem
+            </button>
+          </form>
+        </div>
       </main>
 
     </>
